45810e9e15abb4ebfab5cc9cf608a67471b1e6ca
Nikita <nikita@MacBook-Air-Nikita.local>
2021-11-03 17:02:23 +0300

    add extracting detection

--- DetectionExtractor.py
+++ DetectionExtractor.py
@@ -18,13 +18,19 @@ class DetectionExtractor:
         self._width = None
         self._height = None
 
-    def __coordinate_convert(self):
+    def __xml_to_coco_convert(self):
         self._center_x = self._x_min + ((self._x_max - self._x_min) / 2)
         self._center_y = self._y_min + ((self._x_max - self._y_min) / 2)
         self._height = self._y_max - self._y_min
         self._width = self._x_max - self._x_min
 
-    def extract_data(self, path_to_file: str, convert_coord=False):
+    def __coco_to_xml_convert(self):
+        self._x_min = self._center_x - (self._width / 2)
+        self._x_max = self._center_x + (self._width / 2)
+        self._y_min = self._center_y - (self._height / 2)
+        self._y_max = self._center_y + (self._height / 2)
+
+    def det_yolo_format(self, path_to_file):
         data_from_file = []
         if path_to_file.endswith('.txt'):
             self._label = path_to_file.split('/')[-1].split('.')[0].split('_')[-1]
@@ -37,19 +43,30 @@ class DetectionExtractor:
                     self._y_min = float(split_data[3])
                     self._x_max = float(split_data[4])
                     self._y_max = float(split_data[5])
-
-                    if convert_coord:
-                        self.__coordinate_convert()
-                        data_from_file.append([self._image_name, self._label,
-                                              self._center_x, self._center_y, self._width, self._height,
-                                              self.score])
-                    else:
-                        data_from_file.append([self._image_name, self._label,
-                                              self._x_min, self._y_min, self._x_max, self._y_max,
-                                              self.score])
+                    data_from_file.append([self._image_name, self._label,
+                                          self._x_min, self._y_min, self._x_max, self._y_max,
+                                          self.score])
 
             return data_from_file
-        elif path_to_file.endswith('.json'):
-            pass
+        else:
+            raise RuntimeError("File extension is incorrect")
+
+    def det_class_name_x1y1x2y2(self, path_to_file):
+        data_from_file = []
+        if path_to_file.endswith('.txt'):
+            self._image_name = path_to_file.split('/')[-1].split('.')[0]
+            with open(path_to_file, 'r') as file:
+                for line in file:
+                    split_data = line.strip().split(' ')
+                    self._label = split_data[0]
+                    self.score = float(split_data[1])
+                    self._x_min = float(split_data[2])
+                    self._y_min = float(split_data[3])
+                    self._x_max = float(split_data[4])
+                    self._y_max = float(split_data[5])
+                    data_from_file.append([self._image_name, self._label,
+                                           self._x_min, self._y_min, self._x_max, self._y_max,
+                                           self.score])
+            return data_from_file
         else:
             raise RuntimeError("File extension is incorrect")
--- test.py
+++ test.py
@@ -9,5 +9,6 @@ ae = AnnotationExtractor()
 # print(ae.extract_data('/Users/nikita/Desktop/zip_meters/crop_dataset/00_dataset/meters01/Annotations/mrsk2021_meter01_0000000000.xml'))
 # print(ae.extract_data('/Users/nikita/Desktop/zip_meters/full_dataset/00_dataset/meters01/Annotations/mrsk2021_meter01_0000000000.json'))
 
-# de = DetectionExtractor()
+de = DetectionExtractor()
+de.det_class_name_x1y1x2y2('/Users/nikita/Desktop/zip_meters/result_ed/ed0/05/mrsk2021_meter01_0000000002.txt')
 # print(de.extract_data('/Users/nikita/Desktop/zip_meters/results_yolov4/results_meters_320/comp4_det_test_breaker.txt'))
